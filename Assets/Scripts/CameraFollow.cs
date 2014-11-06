02aa803a26c352ffde923bf088dc58c9e5d24009
Guille <guillermolopezros@gmail.com>
2014-11-06 19:08:16 +0100

    Tarea3-Movimiento de Cámara
    La cámara sigue al personaje.
    Pendiente: terminar de comentar todas las lineras del cámeraFollow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target; // la camara del personaje
+	public float damping = 1;// amortiguacion
+	public float lookAheadFactor = 3;// 
+	public float lookAheadReturnSpeed = 0.5f;//
+	public float lookAheadMoveThreshold = 0.1f;//
+	public float yPosRestiction = -1;//
+	
+	float offsetZ;//
+	Vector3 lastTargetPosition;// estan dado la poscion del perosnaje del ultimo momento
+	Vector3 currentVelocity;// velocidad del personaje
+	Vector3 lookAheadPos;//pocion del personaje
+	
+	// Use this for initialization
+	void Start () {
+		lastTargetPosition = target.position;//
+		offsetZ = (transform.position - target.position).z;//
+		transform.parent = null;//
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// only update lookahead pos if accelerating or changed direction
+		float xMoveDelta = (target.position - lastTargetPosition).x;//
+		
+		bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;//
+		
+		if (updateLookAheadTarget) {//
+			lookAheadPos = lookAheadFactor * Vector3.right * Mathf.Sign(xMoveDelta);//
+		} else {
+			lookAheadPos = Vector3.MoveTowards(lookAheadPos, Vector3.zero, Time.deltaTime * lookAheadReturnSpeed);	//
+		}
+		
+		Vector3 aheadTargetPos = target.position + lookAheadPos + Vector3.forward * offsetZ;//
+		Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref currentVelocity, damping);//
+		
+		newPos = new Vector3(newPos.x, Mathf.Clamp(newPos.y,yPosRestiction,Mathf.Infinity), newPos.z);//
+		transform.position = newPos;
+		
+		lastTargetPosition = target.position;		//
+	}
+}
---
+++ Assets/Scripts/Disparo.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Disparo : MonoBehaviour {
+
+		
+		
+		public GameObject ammo;
+		public Transform puntoDisparo;
+		
+		
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+		
+		public void Fire(){
+			if (ammo != null) {
+				var clone = Instantiate(ammo, puntoDisparo.position,
+				                        Quaternion.identity) as GameObject;
+				clone.transform.localScale = transform.localScale;
+			}
+		}
+	}
