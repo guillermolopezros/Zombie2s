9742fccdd5a114f04f58aca74393fe39aed61df5
guillermolopezros <guillermolopezros@gmail.com>
2014-11-26 18:52:46 +0100

    Vida_disparo

--- Assets/Scripts/JumpScript.cs
+++ Assets/Scripts/JumpScript.cs
@@ -2,23 +2,37 @@
 using System.Collections;
 
 public class JumpScript : MonoBehaviour {
+	
+	public float jumpSpeed = 5f;
+	public float superJump = 2;
+	public bool standing;
+	public GameObject groundCheck;
 
+	
+	// Update is called once per frame
+	void Update () {
+		var absVelY = Mathf.Abs(rigidbody2D.velocity.y);
+		if(absVelY <= .05f){
+			standing = true;
+		}else{
+			standing = false;
+		}
 		
-		public float jumpSpeed = 5f;
-		public bool standing;
-		public GameObject groundCheck;
-		
-		// Update is called once per frame
-		void Update () {
-			var absVelY = Mathf.Abs(rigidbody2D.velocity.y);
-			if(absVelY <= .05f){
-				standing = true;
-			}else{
-				standing = false;
-			}
-			
-			if( (Input.GetKeyDown("up") || Input.GetKeyDown("space")) && standing){
-				rigidbody2D.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
-			}
+		if( (Input.GetKeyDown("up") || Input.GetKeyDown("space")) && standing){
+			rigidbody2D.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
 		}
 	}
+	
+	//Si salimos del trigger cortamos la carga
+	void OnTriggerExit2D(Collider2D target){
+		if (target.transform.tag == "SuperJump") {
+			jumpSpeed = jumpSpeed / superJump;
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D target){
+		if (target.transform.tag == "SuperJump") {
+			jumpSpeed = jumpSpeed * 2;
+		}
+	}
+}
