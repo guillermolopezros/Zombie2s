594a7873ec49be906ea59c1163b97dcd57df192d
guillermolopezros <guillermolopezros@gmail.com>
2014-12-03 18:42:12 +0100

    Pasar de nivel en espera

--- Assets/Scripts/nextLevel.cs
+++ Assets/Scripts/nextLevel.cs
@@ -5,15 +5,15 @@ public class nextLevel : MonoBehaviour {
 	
 	public string nombreNivel = "scene1";// Nombre de la escena que vamos a cargar
 	//public float tiempoEspera = 0.0f; // Tiempo que el Player tiene que estar para cargar
-	
+	public float tiempoEspera = 2f;
 	bool cargando = false; // Controla si el player esta dentro del trigger
 	
 	void OnTriggerStay2D(Collider2D target){
 		
 		if (target.transform.tag == "Player") {
-			Application.LoadLevel(nombreNivel);
-			//if(!cargando) // Si no esta cargando empezamos la cuenta atras
-			//	StartCoroutine(cargaNivel());
+			//Application.LoadLevel(nombreNivel);
+			if(!cargando) // Si no esta cargando empezamos la cuenta atras
+				StartCoroutine(cargaNivel());
 			
 		}
 	}
@@ -24,11 +24,11 @@ public class nextLevel : MonoBehaviour {
 	}
 	
 	// Ejecutamos esto cuando entramos en el trigger
-//	IEnumerator cargaNivel() {
-//		cargando = true;
-//		Debug.Log("Before Waiting 2 seconds");
-//		yield return new WaitForSeconds(tiempoEspera); // Esperamos el tiempo definido
-//		if(cargando) // Si sigue en el trigger cargamos la escena
-//			Application.LoadLevel(nombreNivel);
-//	}
+	IEnumerator cargaNivel() {
+		cargando = true;
+
+		yield return new WaitForSeconds(tiempoEspera); // Esperamos el tiempo definido
+		if(cargando) // Si sigue en el trigger cargamos la escena
+			Application.LoadLevel(nombreNivel);
+	}
 }
