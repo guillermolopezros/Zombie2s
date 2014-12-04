9d5bace80d4b85fcab771dddc19473e37eeca84d
guillermolopezros <guillermolopezros@gmail.com>
2014-12-04 19:38:27 +0100

    MENU CREADO

---
+++ Assets/Scripts/gameControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameControl : MonoBehaviour {
+	public string nombreNivel;
+	public void salir(){
+		Application.Quit ();
+
+	}
+
+
+	public void jugar(){
+		Application.LoadLevel (nombreNivel);
+
+	}	
+}
